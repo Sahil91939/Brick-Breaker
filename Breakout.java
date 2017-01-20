47910ce0a241009a85d5d8cef200766d72389589
S <s>
2017-01-20 17:54:41 +0530

    {"types":"C","runs":19}

--- Breakout.java
+++ Breakout.java
@@ -102,7 +102,7 @@ public class Breakout extends GraphicsProgram {
 	
 	public void mouseMoved(MouseEvent e){
 		if(e.getX()>PADDLE_WIDTH&&e.getX()<(getWidth()-PADDLE_WIDTH)){
-			
+			paddle.setLocation(e.getX(),paddle.getY());
 		}
 	}
 	
