1c57b7a0524a83fc4ad9bc9826ef9023680ff666
S <s>
2017-01-22 16:53:33 +0530

    {"types":"C","runs":24}

--- Breakout.java
+++ Breakout.java
@@ -116,7 +116,6 @@ public class Breakout extends GraphicsProgram {
 	private void checkForCollision(){
 		if(ball.getX()==0||ball.getX()>=(getWidth()-2*BALL_RADIUS)) {
 			vx=-vx;
-			ball.setLocation(ball.getX(),ball.getY());
 		}
 		if(ball.getY()==0||ball.getY()>=(getHeight()-2*BALL_RADIUS))vy=-vy;
 		gobj = getElementAt(ball.getX(),ball.getY()+2*BALL_RADIUS);
