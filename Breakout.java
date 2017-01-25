2ea087bf25cfd74b2a22225cf839c78e4f6f53b2
S <s>
2017-01-25 17:25:50 +0530

    {"types":"C","runs":34}

--- Breakout.java
+++ Breakout.java
@@ -81,7 +81,7 @@ public class Breakout extends GraphicsProgram {
 		while(No_turns!=0){
 			GLabel label = new GLabel("Lives "+No_turns);
 			label.setFont("Times New Roman-16");
-			add(label,0,getHeight()-label.getAscent());
+			add(label,0,getHeight());
 			while(ball.getY()<getHeight()-2*BALL_RADIUS&&No_bricks!=0){
 				ball.move(vx, vy);
 				checkForCollision();
