47b9c916408a8af57ff373bae201c8c7135f6dd3
S <s>
2017-01-25 18:34:02 +0530

    {"types":"C","runs":54}

--- Breakout.java
+++ Breakout.java
@@ -148,7 +148,9 @@ public class Breakout extends GraphicsProgram {
 		
 		if(collider==paddle){
 			vy=-vy;
-			
+			if(ball.getY()>getHeight()-PADDLE_Y_OFFSET-PADDLE_HEIGHT-2*BALL_RADIUS){
+				vx=-vx;
+			}
 		}
 				
 		if(collider==brick){
