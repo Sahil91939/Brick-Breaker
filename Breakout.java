fc6eed1708d2f01c6458389c2ca559d37ed29834
S <s>
2017-01-25 16:33:10 +0530

    {"types":"C","runs":13}

--- Breakout.java
+++ Breakout.java
@@ -63,7 +63,7 @@ public class Breakout extends GraphicsProgram {
 /** Number of turns */
 	private static final int NTURNS = 3;
 //Delay Time in milliseconds
-	private static final int Delay =9;
+	private static final int Delay =10;
 	
 /* Method: run() */
 /** Runs the Breakout program. */
