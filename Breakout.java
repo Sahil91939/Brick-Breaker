c1af2fbff586d29f7dac0980c8cb4d82cfc81235
S <s>
2017-01-25 17:15:39 +0530

    {"types":"C","runs":26}

--- Breakout.java
+++ Breakout.java
@@ -83,7 +83,7 @@ public class Breakout extends GraphicsProgram {
 			pause(Delay);
 		}
 		remove (ball);
-		GLabel Game_Over=new GLabel("Game_Over");
+		GLabel Game_Over=new GLabel("Game Over");
 		Game_Over.setFont("Times New Roman-36");
 		add(Game_Over,getWidth()/2-Game_Over.getWidth()/2,getHeight()/2-Game_Over.getAscent()/2);
 	}
