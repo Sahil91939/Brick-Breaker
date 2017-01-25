abaf1ec5a4dfbc810591a9ddb68ba7a5fddf735f
S <s>
2017-01-25 16:36:17 +0530

    {"types":"C","runs":14}

--- Breakout.java
+++ Breakout.java
@@ -77,7 +77,7 @@ public class Breakout extends GraphicsProgram {
 		add(ball,getWidth()/2-BALL_RADIUS/2,getHeight()/2-BALL_RADIUS/2);
 		vx = rgen.nextDouble(1.0,3.0);
 		if(rgen.nextBoolean(0.5)) vx = -vx;
-		while(ball.getY()<getHeight()-2*BALL_RADIUS){
+		while(ball.getY()<getHeight()-2*BALL_RADIUS||No_bricks==0){
 			ball.move(vx, vy);
 			checkForCollision();
 			pause(Delay);
@@ -146,6 +146,7 @@ public class Breakout extends GraphicsProgram {
 	
 	private GObject getCollidingObject(){
 		if (gobj!=null&&ball.getY()<getHeight()/2){
+			No_bricks--;
 			return brick;
 		}else if(gobj!=null&&ball.getY()>getHeight()/2){
 			return paddle;
