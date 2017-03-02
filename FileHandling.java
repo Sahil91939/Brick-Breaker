95db9af0c0f9fb9b75fef1e759dd8854a727135c
S <s>
2017-03-02 18:03:02 +0530

    {"types":"C","runs":0}

--- FileHandling.java
+++ FileHandling.java
@@ -1,4 +1,19 @@
+import java.io.*;
+import java.util.*;
 
 public class FileHandling {
-
+	
+	private BufferedReader(String prompt){
+		BufferedReader rd =null;
+		while(rd==null){
+			try{
+				String name = readLine(prompt);
+				rd = new BufferedReader(new FileReader(name));
+			}catch(IOException ex){
+				println("bad File");
+			}
+		}
+		return rd ;
+	}
+	
 }
