ee3258b136c7770079d227cef4893efe5851992a
S <s>
2017-03-02 18:16:07 +0530

    {"types":"C","runs":0}

--- FileHandling.java
+++ FileHandling.java
@@ -18,19 +18,20 @@ public class FileHandling extends ConsoleProgram {
 		}
 	return rd ;
 	}
-}
 
-public void run(){
-	BufferedReader rd = openFile("Enter the file name :");
-	
-	try{
-		while(true){
-			String line = rd .readLine();
-			if(line==null)break;
-			println("Read line:["+line+"]");
+
+	public void run(){
+		BufferedReader rd = openFile("Enter the file name :");
+		
+		try{
+			while(true){
+				String line = rd .readLine();
+				if(line==null)break;
+				println("Read line:["+line+"]");
+			}
+			rd.close();
+		}catch(IOException ex){
+			throw new ErrorException(ex);
 		}
-		rd.close();
-	}catch(IOException ex){
-		throw new ErrorException(ex);
 	}
 }
