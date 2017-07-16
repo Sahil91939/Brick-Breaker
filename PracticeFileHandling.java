57b846e97847fbbeba5241fbbe3b99a37e10b50c
S <s>
2017-07-16 15:43:11 +0530

    {"types":"C","runs":0}

--- PracticeFileHandling.java
+++ PracticeFileHandling.java
@@ -1,4 +1,10 @@
+import java.io.*;
+import java.util.*;
+import acm.program.*;
 
-public class PracticeFileHandling {
-
+public class PracticeFileHandling extends consoleProgram {
+	
+	private BufferedReader openFile(String prompt){
+		
+	}
 }
