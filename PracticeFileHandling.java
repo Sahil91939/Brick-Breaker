592fc3aaed46d9842a4a6e736b6ae5d487b6e17c
S <s>
2017-07-16 16:01:55 +0530

    {"types":"C","runs":0}

--- PracticeFileHandling.java
+++ PracticeFileHandling.java
@@ -1,10 +1,36 @@
 import java.io.*;
-import java.util.*;
 import acm.program.*;
+import acm.util.*;
 
-public class PracticeFileHandling extends consoleProgram {
+public class PracticeFileHandling extends ConsoleProgram {
 	
 	private BufferedReader openFile(String prompt){
+		BufferedReader rd = null;
+		
+		while (rd == null){
+			try{
+				String name = readLine(prompt);
+				rd = new BufferedReader ( new FileReader(name));
+			}catch (IOException ex){
+				println("bad file");
+			}
+		}
+	return rd ;
+	}
+	
+	public void run(){
+		BufferedReader xyz = openFile("please enter the file name");
+		
+		try{
+			while(true){
+				String line = xyz.readLine();
+				if(line == null) break ; 
+				println("Read Line :"+line);
+			}
+			xyz.close();
+		}catch (IOException ex){
+			throw new ErrorException(ex);
+		}
 		
 	}
 }
